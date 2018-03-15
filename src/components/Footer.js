3444e34148aaa54f38a27cbc989a376fcb1592fc
Shilpi Verma <shilpiverma@Shilpis-MacBook-Pro.local>
2018-03-15 17:06:11 -0500

    Added react-router and navbar

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import FooterBar from '../style/FooterBar.css'
+
+const Footer = ()=>{
+    return (
+        <div className="footer">
+            <p>Created by Bears-Team-26 for Chingu</p>
+        </div>
+    )
+}
+
+export default Footer;
