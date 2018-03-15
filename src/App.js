c3075900fc720b0d609e122ee3f29d383e4858b9
Sjbrimley26 <sjbrimley26@live.com>
2018-03-14 21:38:23 -0700

    Added Big Cards which you will navigate to when you click a small card.
    Still working on rendering comments, right now it returns a string instead of JSX. Also moved some styled components to a util file.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Flexbox from './components/Flexbox';
+import Flexbox from "./components/Flexbox";
+import BigCard from "./components/BigCard";
+
+const commentsObj = {
+  "John Smith": "Wow what a Cool Bear!"
+};
+
 const App = () => {
   return (
     <div>
       <p>Pinterest!</p>
-      <Flexbox/>
+      <Flexbox />
+      <BigCard
+        imgUrl="https://static.boredpanda.com/blog/wp-content/uploads/2016/09/mother-bear-cubs-animal-parenting-21-57e3a2161d7f7__880.jpg"
+        comments={commentsObj}
+        postedBy="John Smith"
+      />
     </div>
   );
 };
