edd620a0d77e33cb167610d50c90463e393b20cb
Sjbrimley26 <sjbrimley26@live.com>
2018-03-15 21:37:49 -0700

    Added transitions
    Also improved the CommentDiv  component so to be more API compatible

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Flexbox from "./components/Flexbox";
 import BigCard from "./components/BigCard";
+import "./App.css";
 
-const commentsArr = [{
-  "John Smith": "Wow what a Cool Bear!"
-}, {
-  "Steve Johnson": "Such an inspirational photo!"
-}, {
-  "Smarty Pants": `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+const sampleDate = new Date().toDateString();
+
+const commentsArr = [
+  {
+    user: "John Smith",
+    commentText: "Wow what a Cool Bear!",
+    postedOn: sampleDate
+  },
+  {
+    user: "Steve Johnson",
+    commentText: "Such an inspirational photo!",
+    postedOn: sampleDate
+  },
+  {
+    user: "Smarty Pants",
+    postedOn: sampleDate,
+    commentText: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
 
 Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh.
 
@@ -17,7 +29,8 @@ Quisque volutpat condimentum velit. Class aptent taciti sociosqu ad litora torqu
 Quisque cursus, metus vitae pharetra auctor, sem massa mattis sem, at interdum magna augue eget diam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi lacinia molestie dui. Praesent blandit dolor. Sed non quam. In vel mi sit amet augue congue elementum. Morbi in ipsum sit amet pede facilisis laoreet. Donec lacus nunc, viverra nec, blandit vel, egestas et, augue. Vestibulum tincidunt malesuada tellus. Ut ultrices ultrices enim. Curabitur sit amet mauris. Morbi in dui quis est pulvinar ullamcorper.
 
 Nulla facilisi. Integer lacinia sollicitudin massa. Cras metus. Sed aliquet risus a tortor. Integer id quam. Morbi mi. Quisque nisl felis, venenatis tristique, dignissim in, ultrices sit amet, augue. Proin sodales libero eget ante. Nulla quam. Aenean laoreet. Vestibulum nisi lectus, commodo ac, facilisis ac, ultricies eu, pede. Ut orci risus, accumsan porttitor, cursus quis, aliquet eget, justo. Sed pretium blandit orci. Ut eu diam at pede suscipit sodales. `
-}];
+  }
+];
 
 const App = () => {
   return (
@@ -28,7 +41,7 @@ const App = () => {
         imgUrl="https://static.boredpanda.com/blog/wp-content/uploads/2016/09/mother-bear-cubs-animal-parenting-21-57e3a2161d7f7__880.jpg"
         comments={commentsArr}
         postedBy="John Smith"
-        postedOn={new Date().toDateString()}
+        postedOn={sampleDate}
       />
     </div>
   );
