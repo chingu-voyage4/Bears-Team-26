5c0414bfb8a12ea2780529c4382bbafcdeddebe1
Shilpi Verma <shilpiverma@Shilpis-MacBook-Pro.local>
2018-03-18 14:40:52 -0500

    modified navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,37 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
 import logo from '../images/logo.png';
+import twitterLogo from '../images/twitterLogo.png';
 import Navbar from '../style/Navbar.css';
 
-const Header=()=> {
-  return (
-      <header>
-            <div className="header-container">
-                <div className="header-title">
-                    <NavLink to="/">Pinterest
-                    <img className="logo" src={logo}/>
-                    </NavLink>
-                </div>
-                <div className="navbar-links">
+export default class Header extends React.Component {
+    constructor(props){
+        super(props);
+    }
+  render(){
+      const isLogin = this.props.isLogin
+    return (
+        <header>
+              <div className="header-container">
+                  <div className="header-title">
+                      <NavLink to="/">Pinterest
+                      <img className="logo" src={logo}/>
+                      </NavLink>
+                  </div>
+                  <div className="navbar-links">
+                  {!isLogin ?
                     <div className="header-login">
-                    <NavLink to="/login">Login</NavLink>
-                    <NavLink to="/signup">Signup</NavLink>
-                    </div>
-                    <NavLink to ="/search">Search</NavLink> 
-                </div>            
-            </div>
-        </header>
-  )};
+                        <NavLink to="/login">Login with twitter
+                        <img className="logo" src={twitterLogo} />
+                        </NavLink>
+                      </div>
+                    :<Link to="" onClick={this.props.onLogout}>Logout</Link>
 
-   export default Header; 
+                }
+                      <NavLink to ="/search">Search</NavLink> 
+                  </div>            
+              </div>
+          </header>
+    )
+  }
+};
